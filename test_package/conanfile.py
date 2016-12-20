9f4fff659b74a8268e9df01f69f251adfecee79d
SechinM <m.sechin@avantize.com>
2016-12-20 13:46:48 +0200

    Update conanfile.py

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -9,14 +9,15 @@ class RedisReuseConan(ConanFile):
     requires = "redis/1.0.0@%s/%s" % (username, channel)
     generators = "cmake"
 
-    def imports(self):
-      self.copy("*.dll", dst="bin", src="bin")
-      self.copy("*.dylib*", dst="bin", src="lib")
+    #def imports(self):
+    #  self.copy("*.dll", dst="bin", src="bin")
+    #  self.copy("*.dylib*", dst="bin", src="lib")
 
-    def build(self):
-        cmake = CMake(self.settings)
-        self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
-        self.run("cmake --build . %s" % cmake.build_config)
+    #def build(self):
+    #    cmake = CMake(self.settings)
+    #    self.run('cmake "%s" %s' % (self.conanfile_directory, cmake.command_line))
+    #    self.run("cmake --build . %s" % cmake.build_config)
 
     def test(self):
-        self.run("cd bin && .%stestproj" % os.sep)
+        #self.run("cd bin && .%stestproj" % os.sep)
+        self.run("src/redis-server" % os.sep)
